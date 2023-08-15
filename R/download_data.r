9b96208c4a6e6d9aa483e34026d0036a7f9d80ab
miguelta281 <miguelta281@gmail.com>
2023-08-14 19:40:31 -0500

    Update download_data and save_data with service DeviceDataAcces

--- R/download_data.r
+++ R/download_data.r
@@ -14,34 +14,78 @@
 
 download_data <- function(id_device,start_date,end_date,sample_rate,fields = NULL) 
 {
-    download <- function(id_device,start_date,end_date,sample_rate,fields) 
-    {   
+    # Función para manejar las variables
+    convert_measurements <- function(measurements, mode="lower") {
+        # Diccionario de correcciones específicas
+        corrections <- list(
+            temperatura2 = "temperatura_2",
+            humedad2 = "humedad_2",
+            TEMPERATUA2 = "TEMPERATURA_2",
+            HUMEDAD2 = "HUMEDAD_2"
+        )
+        
+        new_measurements <- c()
+  
+        for (measurement in measurements) {
+            # Aplicar correcciones específicas si es necesario
+            if (measurement %in% names(corrections)) {
+                measurement <- corrections[[measurement]]
+            }
+
+            # Convertir a mayúsculas o minúsculas según el modo
+            new_measurement <- ifelse(mode == 'upper', toupper(measurement), tolower(measurement))
+            new_measurements <- c(new_measurements, new_measurement)
+        }
+  
+        return(new_measurements)
+    }
+
+    download <-  function(id_device,start_date,end_date,sample_rate,fields) 
+    {
         library(httr)
         library(dplyr)
+        library(jsonlite)
+
         start_date <- as.numeric(as.POSIXct(start_date))
         end_date <- as.numeric(as.POSIXct(end_date)) 
         datt <- data.frame()
         while (start_date < end_date)
-        {
+        {   
+            # Validar si se piden variables especificas
             if (is.null(fields))
             {
-            url = paste('https://api.makesens.co/ambiental/metricas/',id_device,'/data?agg=1',sample_rate,'&agg_type=mean&items=1000&max_ts=',end_date,'000&min_ts=',start_date,'000',sep='')
-            
+                url = paste('https://api.makesens.co/device/',id_device,'/data?min_ts=',start_date,'&max_ts=',end_date,'&agg=',sample_rate,sep='') 
             }
             else
             {
-            url = paste('https://api.makesens.co/ambiental/metricas/',id_device,'/data?agg=1',sample_rate,'&agg_type=mean&fields=',fields,'&items=1000&max_ts=',end_date,'000&min_ts=',start_date,'000',sep='')                  
+                fields_list <- unlist(strsplit(fields, ","))
+                fields_list <- convert_measurements(fields_list, mode="upper")
+                fields <- paste(fields_list, collapse=",")
+                url = paste('https://api.makesens.co/device/',id_device,'/data?min_ts=',start_date,'&max_ts=',end_date,'&agg=',sample_rate,'&fields=',fields,sep='') 
+            }
+            # Hacer la petición
+            r <- GET(url) # nolint
+            response_text <- content(r, "text" ,encoding = "UTF-8")
+            cleaned_response <- gsub("NaN", "null", response_text)
+            data_list <- fromJSON(cleaned_response)
+            df <- data_list$data %>% as.data.frame()
+            df$ts <- as.POSIXlt(df$ts,origin="1970-01-01")
+
+            # Concatenar las datas
+            if (ncol(datt) == 0) 
+            {
+                datt <- df
+            }
+            else
+            {
+                missing_cols <- setdiff(names(datt), names(df))
+                for(col in missing_cols) {
+                    df[[col]] <- NA # Fill new columns with NAs
+                }
+                datt <- rbind(datt, df)
             }
             
-            r <- GET(url) # nolint
-            datos <- content(r, type = 'application/json', simplifyDataFrame = TRUE)
-            #Manipular los datos
-            dat <- cbind(datos[1]/1000,datos[[2]]) #El primer elemento es el tiempo y el segundo las demas variables
-            #dat <- rename(dat, c(value="ts"))
-            dat$ts <- as.POSIXlt(dat$ts,origin="1970-01-01")
-            t <-  datos[1][[1]][length(datos[1][[1]])] / 1000
-            datt <- bind_rows(datt,dat)
-            
+            t <- data_list$date_range$end
 
             if (toString(t) == start_date)
             {
@@ -52,9 +96,14 @@ download_data <- function(id_device,start_date,end_date,sample_rate,fields = NUL
             {
                 start_date <- toString(t)
             }
+            
         }
+        # Eliminar los repetidos
+        datt <- datt %>% distinct(ts, .keep_all = TRUE)
+        colnames(datt) <- convert_measurements(colnames(datt), mode="lower")
         return(datt)
     }
     
-    tryCatch(download(id_device,start_date,end_date,sample_rate,fields),error = function(e) message('No hay datos en este intervalo'))
+    download(id_device,start_date,end_date,sample_rate,fields)
 }
+
--- R/save_data.r
+++ R/save_data.r
@@ -13,53 +13,118 @@
 #' @examples
 #' save_data()
 
-save_data <- function(id_device,start_date,end_date,sample_rate,format,fields=NULL) 
-{
-    library(httr)
-    library(dplyr)
-    library(xlsx)
-    start_date <- as.numeric(as.POSIXct(start_date))
-    end_date <- as.numeric(as.POSIXct(end_date))
-    datt <- data.frame()
-    while (start_date < end_date)
-    {
-        if (is.null(fields))
-        {
-        url = paste('https://api.makesens.co/ambiental/metricas/',id_device,'/data?agg=1',sample_rate,'&agg_type=mean&items=1000&max_ts=',end_date,'000&min_ts=',start_date,'000',sep='')
-        }
-        else
-        {
-        url = paste('https://api.makesens.co/ambiental/metricas/',id_device,'/data?agg=1',sample_rate,'&agg_type=mean&fields=',fields,'&items=1000&max_ts=',end_date,'000&min_ts=',start_date,'000',sep='')                  
-        }
-        #Descargar los datos
-        r <- GET(url)
-        datos <- content(r, type = 'application/json', simplifyDataFrame = TRUE)
-        #Manipular los datos
-        dat <- cbind(datos[1]/1000,datos[[2]]) #El primer elemento es el tiempo y el segundo las demas variables
-        dat$ts <- as.POSIXlt(dat$ts,origin="1970-01-01")
-        t <-  datos[1][[1]][length(datos[1][[1]])] / 1000
-        datt <- bind_rows(datt,dat)
-        
 
-        if (toString(t) == start_date)
-        {
-            start_date <- end_date
-        }
+save_data <- function(id_device,start_date,end_date,sample_rate,format,fields = NULL) 
+{
+    library(xlsx)
+    # Función para manejar las variables
+    convert_measurements <- function(measurements, mode="lower") {
+        # Diccionario de correcciones específicas
+        corrections <- list(
+            temperatura2 = "temperatura_2",
+            humedad2 = "humedad_2",
+            TEMPERATUA2 = "TEMPERATURA_2",
+            HUMEDAD2 = "HUMEDAD_2"
+        )
         
-        else
-        {
-            start_date <- toString(t)
+        new_measurements <- c()
+  
+        for (measurement in measurements) {
+            # Aplicar correcciones específicas si es necesario
+            if (measurement %in% names(corrections)) {
+                measurement <- corrections[[measurement]]
+            }
+
+            # Convertir a mayúsculas o minúsculas según el modo
+            new_measurement <- ifelse(mode == 'upper', toupper(measurement), tolower(measurement))
+            new_measurements <- c(new_measurements, new_measurement)
         }
+  
+        return(new_measurements)
+    }
+
+    download <-  function(id_device,start_date,end_date,sample_rate,fields) 
+    {
+        library(httr)
+        library(dplyr)
+        library(jsonlite)
+
+        start_date <- as.numeric(as.POSIXct(start_date))
+        end_date <- as.numeric(as.POSIXct(end_date)) 
+        datt <- data.frame()
+        while (start_date < end_date)
+        {   
+            # Validar si se piden variables especificas
+            if (is.null(fields))
+            {
+                url = paste('https://api.makesens.co/device/',id_device,'/data?min_ts=',start_date,'&max_ts=',end_date,'&agg=',sample_rate,sep='') 
+            }
+            else
+            {
+                fields_list <- unlist(strsplit(fields, ","))
+                fields_list <- convert_measurements(fields_list, mode="upper")
+                fields <- paste(fields_list, collapse=",")
+                url = paste('https://api.makesens.co/device/',id_device,'/data?min_ts=',start_date,'&max_ts=',end_date,'&agg=',sample_rate,'&fields=',fields,sep='') 
+            }
+            # Hacer la petición
+            r <- GET(url) # nolint
+            response_text <- content(r, "text" ,encoding = "UTF-8")
+            cleaned_response <- gsub("NaN", "null", response_text)
+            data_list <- fromJSON(cleaned_response)
+            df <- data_list$data %>% as.data.frame()
+            df$ts <- as.POSIXlt(df$ts,origin="1970-01-01")
+
+            # Concatenar las datas
+            if (ncol(datt) == 0) 
+            {
+                datt <- df
+            }
+            else
+            {
+                missing_cols <- setdiff(names(datt), names(df))
+                for(col in missing_cols) {
+                    df[[col]] <- NA # Fill new columns with NAs
+                }
+                datt <- rbind(datt, df)
+            }
+            
+            t <- data_list$date_range$end
+
+            if (toString(t) == start_date)
+            {
+                start_date <- end_date
+            }
+
+            else
+            {
+                start_date <- toString(t)
+            }
+            
+        }
+        # Eliminar los repetidos
+        datt <- datt %>% distinct(ts, .keep_all = TRUE)
+        colnames(datt) <- convert_measurements(colnames(datt), mode="lower")
+        return(datt)
     }
     
+    datt <- download(id_device,start_date,end_date,sample_rate,fields)
     if (format == 'csv')
     {
-        name <- paste(id_device,'_',datt$ts[1],'_',as.Date(as.POSIXlt(end_date,origin = '1970-01-01')),'.csv',sep='')
+        name <- paste(id_device, 
+              format(as.POSIXct(datt$ts[1]), "%Y-%m-%d_%H-%M-%S"), 
+              format(as.Date(as.POSIXlt(end_date, origin = '1970-01-01')), "%Y-%m-%d"), 
+              '.csv', sep='_')
         write.csv(datt,name)
     }
     else if (format == 'xlsx')
     {
-        name <- paste(id_device,'_',datt$ts[1],'_',as.Date(as.POSIXlt(end_date,origin = '1970-01-01')),'.xlsx',sep='')
+        name <- paste(id_device, 
+              format(as.POSIXct(datt$ts[1]), "%Y-%m-%d_%H-%M-%S"), 
+              format(as.Date(as.POSIXlt(end_date, origin = '1970-01-01')), "%Y-%m-%d"), 
+              '.xlsx', sep='_')
         write.xlsx(datt,name)
     }
 }
+
+
+
